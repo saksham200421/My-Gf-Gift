9ebf4baf3556d2872b0b3e8ca9446d0d8337ce78
saksham <74900810+saksham200421@users.noreply.github.com>
2026-02-14 23:17:48 +0530

    refactor: remove unnecessary whitespace in MarryMe component

--- frontend/src/pages/MarryMe.jsx
+++ frontend/src/pages/MarryMe.jsx
@@ -275,28 +275,6 @@ function MarryMe() {
           <button
             key={`no-${noChaosTick}`}
             type="button"
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-     . 
             className="marry-choice-no is-chaos"
             onClick={handleNoClick}
           >
